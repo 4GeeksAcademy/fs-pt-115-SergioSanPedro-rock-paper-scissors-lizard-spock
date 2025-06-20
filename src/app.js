90ded196decc89f55a0599eb065944d039f26c3e
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-06-20 12:24:07 +0000

    sin terminar

--- src/app.js
+++ src/app.js
@@ -1,3 +1,59 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+
+
+const palabras = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+const jugadaGanadora = ['rock-scissors',
+    'rock-lizards',
+    `paper-rock`,
+    'paper-spock',
+    'scissors-paper',
+    'scissors-lizard',
+    'lizard-paper',
+    'lizard-spok',
+    'spok-scissors',
+    'spok-rock']
+
+
+const machineChoose = (palabras) => {
+
+    let index = Math.floor(Math.random() * palabras.length)
+    return palabras[index]
+
+}
+
+function checkGuess() {
+    
+    let userChoose = prompt('elige entre estas opciones: rock, paper, scissors, lizard, spock').toLowerCase();
+
+    console.log(`User choose: ${userChoose}`);
+    
+
+    let computer = machineChoose(palabras);
+
+    console.log(`Machine choose: ${computer}`);
+    
+    let play = userChoose + '-' + computer
+
+    if (userChoose === computer) {
+        console.log('It`s a draw!!');
+         
+
+    }else if (jugadaGanadora.includes(play)) {
+        console.log(`You win`);
+        
+    }else {
+        console.log('You loose');
+        
+    }
+
+}
+
+
+checkGuess()
+ 
+
+
+
+
 
